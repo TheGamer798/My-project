332048cb34c3080f40829738299859fc88d2149b
TheGamer798 <justycem8@gmail.com>
2024-03-26 10:45:43 -0500

    collisions

--- Assets/Scripts/FlappyBird.cs
+++ Assets/Scripts/FlappyBird.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class FlappyBird : MonoBehaviour
 {
     public GameObject Ship;
+    public GameObject PipePrefab;
     public float Gravity = 30;
     public float Jump = 10;
+    public float PipeSpawnInterval = 2;
+    public float PipeSpeed = 5;
 
     private float VerticalSpeed;
+    private float PipeSpawnCountdown;
+    private GameObject PipesHolder;
+    private int PipeCount;
 
     void Start()
     {
-        
+        PipeCount = 0;
+        Destroy(PipesHolder);
+        PipesHolder = new GameObject("PipesHolder");
+        PipesHolder.transform.parent = this.transform;
+
+        VerticalSpeed = 0;
+        Ship.transform.position = Vector3.up * 1;
+
+        PipeSpawnCountdown = 0;
     }
 
     
@@ -27,5 +41,27 @@ public class FlappyBird : MonoBehaviour
         }
 
         Ship.transform.position += Vector3.up * VerticalSpeed * Time.deltaTime;
+
+        PipeSpawnCountdown -= Time.deltaTime;
+
+        if (PipeSpawnCountdown <= 0 )
+        {
+            PipeSpawnCountdown = PipeSpawnInterval;
+
+            GameObject pipe = Instantiate(PipePrefab);
+            pipe.transform.parent = PipesHolder.transform;
+            pipe.transform.name = (++PipeCount).ToString();
+
+            pipe.transform.position += Vector3.right * 30;
+            pipe.transform.position -= Vector3.up * Mathf.Lerp(4, 9, Random.value);
+        }
+
+        PipesHolder.transform.position += Vector3.left * PipeSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+
+        Start();
     }
 }
